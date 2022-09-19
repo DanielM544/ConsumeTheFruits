7cc28ff562394e25cea278906f27709e40db165b
Daniel Medcalf <danielmedcalf@mmc.school.nz>
2022-09-19 19:40:17 +1200

    Drawn 10 Fruits on Screen, All move down screen, and are moved back to top if reach bottom.

---
+++ ConsumeTheFruits/FrmConsume.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsumeTheFruits
+{
+    public partial class FrmConsume : Form
+    {
+        Graphics g; //declare a graphics object called g
+
+        // declare space for an array of 7 objects called Fruit 
+        Fruit[] fruit = new Fruit[10];
+
+
+        public FrmConsume()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                int x = 40 + (i * 75);
+                fruit[i] = new Fruit(x);
+
+            }
+
+        }
+
+        private void FrmConsume_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PnlGame_Paint(object sender, PaintEventArgs e)
+        {
+            //get the graphics used to paint on the panel control
+            g = e.Graphics;
+            for (int i = 0; i < 10; i++)
+            {
+                //call the Planet class's drawPlanet method to draw the images
+                fruit[i].DrawFruit(g);
+            }
+
+
+        }
+
+        private void TmrPlanet_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                fruit[i].MoveFruit();
+
+                //if a planet reaches the bottom of the Game Area reposition it at the top
+                if (fruit[i].y >= PnlGame.Height)
+                {
+                    fruit[i].y = 30;
+                }
+
+            }
+            PnlGame.Invalidate();//makes the paint event fire to redraw the panel
+
+        }
+    }
+}
---
+++ ConsumeTheFruits/Fruit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace ConsumeTheFruits
+{
+    class Fruit
+    {
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the rectangle
+        public Image fruitImage;//variable for the planet's image
+
+        public Rectangle fruitRec;//variable for a rectangle to place our image in
+        public int score;
+        //Create a constructor (initialises the values of the fields)
+        public Fruit(int spacing)
+        {
+            x = spacing;
+
+            y = 10;
+            width = 20;
+            height = 20;
+            //planetImage contains the plane1.png image
+            fruitImage = Properties.Resources.fruit1;
+            fruitRec = new Rectangle(x, y, width, height);
+        }
+        // Methods for the Planet class
+        public void DrawFruit(Graphics g)
+        {
+            fruitRec = new Rectangle(x, y, width, height);
+            g.DrawImage(fruitImage, fruitRec);
+        }
+        public void MoveFruit()
+        {
+            y += 5;
+
+            fruitRec.Location = new Point(x, y);
+        }
+
+    }
+}
